1f9d47fed21d9d803498def10b67512b40a396dd
sheko <shekohaha2004@gmail.com>
2026-02-10 05:11:29 +0200

    finishing Updat icons and android:name app

--- lib/core/assets_path/assets_path.dart
+++
@@ -1,8 +0,0 @@
-abstract class AssetsPath {
-  static const String quranActiveIcon = "assets/icons/quran_active_icn.svg";
-  static const String quranInActiveIcon =
-      "assets/icons/quran_in_active_icn.svg";
-  static const String hadithActiveIcon = "assets/icons/hadith_active_icn.svg";
-  static const String hadithInActiveIcon =
-      "assets/icons/hadith_in_active_icn.svg";
-}
